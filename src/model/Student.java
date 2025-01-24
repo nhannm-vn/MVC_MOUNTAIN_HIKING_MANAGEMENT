636b601493b0f57518cd74a4ca00294f03022ca3
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-01-24 11:44:33 +0700

    update constructor discount money for class Student

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,7 @@
 package model;
 
+import tools.Acceptable;
+
 
 public class Student {
     // properties
@@ -19,13 +21,33 @@ public class Student {
     // Constructor full-field
 
     public Student(String id, String name, String phone, String email, String mountainCode, double tutionFee) {
-        this.id = id;
+        this.id = id.toUpperCase();
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.mountainCode = mountainCode;
         this.tutionFee = tutionFee;
     }
+    
+    // Constructor3: tính số tiền sẵn cho sinh viên dựa vào nhà mạng
+
+    public Student(String id, String name, String phone, String email, String mountainCode) {
+        this.id = id.toUpperCase();
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.mountainCode = mountainCode;
+        if(Acceptable.isValid(this.phone, Acceptable.VNPT_VALID) 
+           || Acceptable.isValid(this.phone, Acceptable.VIETTEL_VALID)){
+            double free = (6000000 / 100) * 35;
+            this.tutionFee = 6000000 - free;
+        }else{
+            this.tutionFee = 6000000;
+        }
+    }
+    
+    
+    
     
     // Getter and Setter
 
