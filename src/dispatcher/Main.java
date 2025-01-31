88afbf09165b9a7c55094312201eeb4f769b4766
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-01-31 08:46:50 +0700

    done setup menu

--- src/dispatcher/Main.java
+++ src/dispatcher/Main.java
@@ -38,7 +38,43 @@ public class Main {
         // Tạo biến để thu thập lựa chọn của người dùng
         int choice;
         while (true) {
+            // show menu
+            menu.print();
+            // moi nhap lua chon
             choice = menu.getChoice();
+            switch (choice) {
+                case 1: {
+                    break;
+                }
+                case 2: {
+                    break;
+                }
+                case 3: {
+                    break;
+                }
+                case 4: {
+                    break;
+                }
+                case 5: {
+                    break;
+                }
+                case 6: {
+                    break;
+                }
+                case 7: {
+                    break;
+                }
+                case 8: {
+                    break;
+                }
+                case 9: {
+                    return;
+                }
+                default: {
+                    System.out.println("This function is not available.");
+                    break;
+                }
+            }
         }
 
     }
