b00ba5af4633bbbd69cd85bdd3eaf0dde4e2220e
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-01-31 08:28:57 +0700

    set up main

--- src/dispatcher/Main.java
+++ src/dispatcher/Main.java
@@ -1,13 +1,19 @@
 package dispatcher;
 
 import business.Mountains;
+import business.Students;
 import java.util.ArrayList;
 import model.Mountain;
 import model.Student;
+import tools.Menu;
 
 
 public class Main {
     public static void main(String[] args) {
+        // Tạo instance Students chuyên quản lí các chức năng liên quan đến danh sách
+        Students sm = new Students();
+        // Tạo instance Menu chuyên quản lí danh sách menu
+        Menu menu = new Menu("Mountain Hiking Application");
         
     }
 }
