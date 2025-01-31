4cedc3c34864fae263303b28e3e5872a83781f78
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-01-31 08:32:46 +0700

    set up main

--- src/dispatcher/Main.java
+++ src/dispatcher/Main.java
@@ -5,6 +5,7 @@ import business.Students;
 import java.util.ArrayList;
 import model.Mountain;
 import model.Student;
+import tools.Inputter;
 import tools.Menu;
 
 
@@ -14,6 +15,20 @@ public class Main {
         Students sm = new Students();
         // Tạo instance Menu chuyên quản lí danh sách menu
         Menu menu = new Menu("Mountain Hiking Application");
+        // Tạo instance Input chuyên quản lí nhập dữ liệu
+        Inputter input = new Inputter();
+        // Tạo instance Mountains chuyên quản lí các mountain
+        Mountains mountains = new Mountains();
         
+        // Thêm yêu các yêu cầu cho menu
+        menu.addNewOption("Add a new student registration.");
+        menu.addNewOption("Modify existing registration details.");
+        menu.addNewOption("Show the list of all registered students.");
+        menu.addNewOption("Remove a student's registration record.");
+        menu.addNewOption("Find registered students based on their names.");
+        menu.addNewOption("Display registrations specific to a campus");
+        menu.addNewOption("Generate statistics on the number of registrations for each location.");
+        menu.addNewOption("Store registration data in a file");
+        menu.addNewOption("End the program execution.");
     }
 }
